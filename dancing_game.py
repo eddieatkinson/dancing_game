c5d05ed6a46a21a40910973fcba8e6af7d39d726
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-09-19 14:45:01 -0400

    Dancing game

--- dancing_game.py
+++ dancing_game.py
@@ -21,7 +21,7 @@ ouch = pygame.mixer.Sound("ouch.wav")
 
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("Level 1: Avoid Ginger!")
-level3_img = ["fred3.png", "fred2.png", "fred1.png"]
+level3_img = ["fred4.png", "fred3.png", "fred2.png", "fred1.png", "fall.png", "fred1.png"]
 img_counter = 0
 dance_moves = ["a", "s", "d", "f"]
 keys = {
@@ -61,6 +61,8 @@ time_run = 0
 num_incorrect = 0
 win = True
 
+correct_text_msg = ""
+
 def lost():
 	pygame.mixer.music.stop()
 	pygame.mixer.Sound.play(pygame.mixer.Sound('loser.wav'))
@@ -70,7 +72,8 @@ pygame.mixer.music.play(-1)
 while game_on: # Will run forever (until break)
 	# Loop through all the pygame events.
 	# This is pygame's escape hatch (quit).
-	while player.wins < 1:
+	# ----LEVEL 1----
+	while player.wins < 11:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				quit()
@@ -84,9 +87,6 @@ while game_on: # Will run forever (until break)
 					player.should_move("right", True)
 				elif event.key == 276:
 					player.should_move("left", True)
-				# elif event.key == 32: # Spacebar...FIRE!
-				# 	new_bullet = Bullet(screen, player)
-				# 	bullets.add(new_bullet)
 			elif event.type == pygame.KEYUP:
 				if event.key == 273:
 					player.should_move("up", False)
@@ -97,7 +97,6 @@ while game_on: # Will run forever (until break)
 				elif event.key == 276:
 					player.should_move("left", False)
 
-		# while score < 10:
 		# Paint the screen (no "blit" needed, as it's not an image).
 		screen.blit(background_image1, [0, 0])
 		
@@ -107,7 +106,6 @@ while game_on: # Will run forever (until break)
 			villain.draw_me()
 
 		# Must be after fill, or we won't be able to see the hero.
-		# screen.blit(the_player_image, [player.x, player.y])
 		player.draw_me()
 
 		coin.draw_coins()
@@ -134,9 +132,7 @@ while game_on: # Will run forever (until break)
 			villain.speed_up()
 			player.speed_up()
 			pygame.mixer.Sound.play(yeah)
-		#	coins.add(Coins(screen, ))
 		
-		# coin.draw_coins()
 		# Flip the screen, i.e. clear it, so we can draw again...and again...and again
 		if (player.y > 800 - 100):
 			player.y = 0
@@ -147,11 +143,12 @@ while game_on: # Will run forever (until break)
 		elif (player.x < 0):
 			player.x = 1000 - 100
 
-		# divisor -= .01
 		pygame.display.flip()
-	if player.wins == 1:
+	
+	# ----LEVEL 2----
+	if player.wins == 11:
 		clock_ticks = pygame.time.get_ticks() # To initiate the timer for the "seconds" variable below.
-	while player.wins >= 1 and seconds <= 3:
+	while player.wins >= 11 and seconds <= 15:
 		player.change_image(pygame.image.load("fred2.png"), 100, 100)
 		pygame.display.set_caption("Level 2: Rotten Tomatoes!")
 		screen.blit(background_image2, [0, 0])
@@ -192,33 +189,27 @@ while game_on: # Will run forever (until break)
 			pygame.mixer.Sound.play(ouch)
 			shot = True
 
-			# seconds = clock_ticks
 
 		if count % int((divisor + 1)) == 0:
 			new_bullet = Bullet(screen, player, bull_speed)
 			# new_bullet.gain_speed()
 			bullets.add(new_bullet)
 			bull_speed += 1
-		# clock_ticks = pygame.time.get_ticks()
 		
 		# while not shot:
 		seconds = (pygame.time.get_ticks() - clock_ticks)/ 1000
-		# ticker = (pygame.time.get_ticks() - clock_ticks)/ 1000 - seconds
 		font_time = pygame.font.Font(None, 40)
 		time_text = font_time.render("Time: %s" % seconds, True, (0, 0, 0))
 		screen.blit(time_text, [40, 40])
 
-		# while timer < 20:
-		# 	print timer
-		# 	timer += 1
-		# 	pygame.time.delay(10)
 		count += 1
 		divisor /= 1.01
 		player.draw_me()
 
 		pygame.display.flip()
 
-	while seconds >= 3:
+	# ----LEVEL 3----
+	while seconds >= 15:
 		player.change_image(pygame.image.load(level3_img[img_counter]), 400, 400)
 		player.new_position(400, 0)
 		pygame.display.set_caption("Level 3: Dance Off!")
@@ -229,44 +220,40 @@ while game_on: # Will run forever (until break)
 	 	correct_move = False
 	 	font_move = pygame.font.Font(None, 40)
 		current_move = dance_moves[move]
-		print current_move
+
+		correct_text = font_move.render("%s" % correct_text_msg, True, (0, 255, 0))
+		screen.blit(correct_text, [80, 100])
+
 		dir_text = font_move.render("Dance move: %s" % current_move, True, (0, 0, 0))
+	 	print current_move
 	 	screen.blit(dir_text, [700, 700])
 		
-		while not correct_move and time_run != 0:
-			# correct_move = True
-			# dir_text = font_time.render("Dance move: %s" % dance_moves[move], True, (0, 0, 0))
-	 	# 	screen.blit(dir_text, [700, 700])
+		while not correct_move: 
 		 	for event in pygame.event.get():
 		 		if event.type == pygame.QUIT:
 		 			quit()
 			 	if event.type == pygame.KEYDOWN:
 			 		if event.key == keys[dance_moves[move]]:
-			 			correct_text = font_move.render("Great job! Keep it up!", True, (255, 0, 0))
-			 			screen.blit(correct_text, [100, 100])
+			 			correct_text_msg = "Great job! Keep it up!"
 			 			correct_move = True
 			 			old_img_counter = img_counter
-			 			img_counter = randint(0, 2)
+			 			img_counter = randint(0, 3)
 			 			if old_img_counter == img_counter:
 			 				img_counter -= 1
-			 			# level3_img = "fred2.png"
-			 			# player.change_image(pygame.image.load("fred2.png"), 400, 400)
-			 			# player.new_position(0, 0)
-			 			# player.draw_me()
 			 		else:
-			 			incorrect_text = font_move.render("Try again!", True, (255, 255, 255))
-			 			screen.blit(incorrect_text, [300, 300])
 			 			correct_move = False
-			 			num_incorrect += 1
-			 			# player.change_image(pygame.image.load("fred2.png"), 400, 400)
-			 			# player.draw_me()
-			 		# else:
-			 		# 	correct_move = False
-			# screen.blit(dir_text, [700, 700])
+			 			img_counter = 4
+			 			correct_text_msg = "Try again!"
+			 			correct_text = font_move.render("%s" % correct_text_msg, True, (255, 0, 0))
+			 			screen.blit(correct_text, [80, 100])
+			 			player.change_image(pygame.image.load(level3_img[img_counter]), 400, 400)
+			 			player.draw_me()
+			 			num_incorrect += 1 			
 			if num_incorrect == 10:
 				game_over_text = font_move.render("Game over!", True, (255, 255, 255))
 			 	screen.blit(game_over_text, [400, 100])
-		
+			pygame.display.flip()
+
 	 	time_run += 1
 		move = randint(0, 3)
 		pygame.display.flip()
